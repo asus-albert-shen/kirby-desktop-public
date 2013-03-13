d5aea35f334b0534edfe14d87f74557a4ce1ad7e
Bradley Grzesiak <brad@bendyworks.com>
2013-03-13 15:58:23 -0500

    Use "original-shot" instead of "original"

--- scripts/pages/project_show.ts
+++ scripts/pages/project_show.ts
@@ -15,12 +15,12 @@ $$('.project') {
 
     insert('div', class: 'image-toggle-control', data-image-toggle: '#shots') {
       insert('a', 'Remixed', href: '#', data-image: 'full-shot', class: 'selected')
-      insert('a', 'Original', href: '#', data-image: 'original')
+      insert('a', 'Original', href: '#', data-image: 'original-shot')
     }
 
     insert('div', class: 'image-toggle', id: 'shots') {
       move_here('../../img[@data-type="full-shot"]') { add_class('selected') }
-      move_here('../../img[@data-type="original"]')
+      move_here('../../img[@data-type="original-shot"]')
     }
 
     move_here('../section[@data-type="extended-description"]')
