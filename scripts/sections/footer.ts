c7a3162f62e26b8503278daf07386a2c7f95e307
Bradley Grzesiak <brad@bendyworks.com>
2013-03-14 10:14:17 -0500

    Finish up draft of front page

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -3,3 +3,20 @@
 #     Move stuff here
 #   }
 # }
+
+$$('body') {
+  insert_top('div', id: 'root') {
+    move_here('../header')
+    move_here('../div[@id="main"]')
+    insert('div', id: 'root_footer')
+  }
+}
+
+$$('footer.page') {
+  insert('div', class: 'footer-wrapper') {
+    move_here('../p[@data-type="copyright"]')
+    insert('div', class: 'buttons') {
+      move_here('../../*[contains(@class, "btn")]')
+    }
+  }
+}
